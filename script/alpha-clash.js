3c7be800e46e67bae42a2ceced81c7978503cff9
misskhat <misskhat.hossen@gmail.com>
2024-05-02 03:38:47 +0600

    function screen and keyboard work done

--- script/alpha-clash.js
+++ script/alpha-clash.js
@@ -7,6 +7,9 @@ function play(){
 function continueGame(){
     // generate a random alphabet;
     const alphabetOnScreen = getARandomAlphabets()
-    console.log(alphabetOnScreen)
-
+    console.log(alphabetOnScreen);
+    // show random alphabets to screen;
+    const currentAlphabet = document.getElementById('current-alphabet');
+    currentAlphabet.innerText = alphabetOnScreen;
+    setBackgroundColorById(alphabetOnScreen)
 }
--- script/uitility.js
+++ script/uitility.js
@@ -8,6 +8,16 @@ function showElementByID(elementId){
     element.classList.remove('hidden');
 }
 
+function setBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-orange-400');
+}
+
+function removeBackgroundColor(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-400')
+}
+
 function getARandomAlphabets(){
     const alphabetsString = 'abcdefghijklmnopqrstuvwxyz';
     const alphabets = alphabetsString.split('');
